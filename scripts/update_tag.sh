df3badb4999cf25a9e38543d37cab8d99375c816
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-06 11:57:16 +0530

    fix(fix): fix

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-echo "abc"
 echo $GITHUB_REF
+splitRef=$(echo $IN | tr "/" "\n")
+branch = ${splitRef[-1]}
+
+echo $branch
+
+cat ../package.json | jq \
+ '.publishConfig.tag |= $branch' > "$tmp" && mv "$tmp" ../package.json
+
+ cat ../package.json
