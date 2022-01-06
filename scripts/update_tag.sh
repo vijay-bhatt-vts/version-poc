c660d579352b94e8a9179783dedc9ce61bdcb915
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-06 12:15:40 +0530

    fix(fic): fix

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -3,13 +3,20 @@
 set -e
 
 echo $GITHUB_REF
+echo ${GITHUB_REF##*/}
 splitRef=$(echo $GITHUB_REF | tr "/" "\n")
 echo $splitRef
 branch=${splitRef[${#splitRef[@]} - 1]}
 
 echo $branch
 
-cat ../package.json | jq \
- '.publishConfig.tag |= $branch' > "$tmp" && mv "$tmp" ../package.json
+project_root=$(dirname $(dirname $(dirname $(realpath $0 ))))
+echo $project_root
 
- cat ../package.json
+# data_dir="$project_root/examples/data"
+# echo "DATA: $data_dir"
+
+# cat ../package.json | jq \
+#  '.publishConfig.tag |= $branch' > "$tmp" && mv "$tmp" ../package.json
+
+#  cat ../package.json
