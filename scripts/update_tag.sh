8d0f84a493c8994ab43095b660cd35d556ba9a60
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-06 13:13:25 +0530

    fix(y): y

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -10,11 +10,7 @@ echo $branch
 project_root=$(dirname $(dirname $(dirname $(realpath $0 ))))
 echo $project_root
 
-var data = require('./${project_root/version-poc/package.json}');
 
-data.publishConfig.tag = $branch;
-
-console.log(JSON.stringify(data));
 
 # cat $project_root/version-poc/package.json | jq \
 #   --arg tagname $branch \
