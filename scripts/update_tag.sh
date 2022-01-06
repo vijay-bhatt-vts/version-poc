41b6fdd0a741fd1cb4128a5d7eb1ce9a6f5cfd99
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-06 12:28:22 +0530

    fix(4): 4

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -8,6 +8,12 @@ branch=${GITHUB_REF##*/}
 echo $branch
 
 project_root=$(dirname $(dirname $(dirname $(realpath $0 ))))
+echo $project_root
+
+ls $project_root/version-poc
+
+# data_dir="$project_root/examples/data"
+# echo "DATA: $data_dir"
 
 cat $project_root/version-poc/package.json | jq \
  '.publishConfig.tag |= $branch' > "$tmp" && mv "$tmp" ../package.json
