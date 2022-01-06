e1c62cfa5e698728e007f6ce5f39f9c3b2d721a7
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-06 12:33:55 +0530

    fix(7): 7

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -17,7 +17,7 @@ ls $project_root/version-poc
 
  cat $project_root/version-poc/package.json
 
-# cat $project_root/version-poc/package.json | jq \
-#  '.publishConfig.tag |= $branch' > "$tmp" && mv "$tmp" $project_root/version-poc/package.json
+cat $project_root/version-poc/package.json | jq \
+ '.publishConfig.tag |= $branch' > "$tmp" && mv "$tmp" $project_root/version-poc/package.json
 
-#  cat $project_root/version-poc/package.json
+cat $project_root/version-poc/package.json
