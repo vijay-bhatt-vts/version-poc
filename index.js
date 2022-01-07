a8605357bb7aceae16ea5075d3943c1586ed7cc1
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-07 15:12:32 +0530

    feat(breaking): breaking
    BREAKING CHANGE: breaking change!!!

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports.helloWorld = function () {
 
     console.log("Hello world main latest 123");
 
-    console.log("major change in staging 1")
+    console.log("major change in staging 1 breaking")
 
   };
