f60b13297cd0cc3c7f71c4367b760a49c6deaab4
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-07 15:35:13 +0530

    feat(checking tags): checking tags

---
+++ scripts/update_tags.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo $GITHUB_REF
+branch=${GITHUB_REF##*/}
+
+echo $branch
+
+lasttag=git tag | grep $branch | tail -1
+
+echo $lasttag
+
+git tag -d $lasttag
+git push --delete origin $lasttag  
+git tag $lasttag
+git push origin $lasttag
+
+git notes --ref semantic-release add -f -m '{"channels":${branch}}' $lasttag
+git push --force origin refs/notes/semantic-release
