021a0d7e291bc90c5741c7e656aadfdf2238d30d
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-07 07:33:15 +0530

    feat(check): check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("check pull request");
 
 module.exports.helloWorld = function () {
 
-    console.log("Hello world main 123");
+    console.log("Hello world main");
 
     console.log("Preprod fix")
 
