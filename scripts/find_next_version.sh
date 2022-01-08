0003844610853f13531f7baac18ddad121b93376
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-09 00:09:32 +0530

    feat(check): check

--- scripts/find_next_version.sh
+++ scripts/find_next_version.sh
@@ -1,4 +1,11 @@
-npx semantic-release --ci false --dryRun | grep -i "Published release" > .semver-output
 
-export LATEST_VERSION=$([[ $(cat .semver-output) =~ .*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+) ]] && echo ${BASH_REMATCH[1]})
+LATEST_VERSION=$(cat .semver-output | cut -d " " -f 8)
 echo "latest version $LATEST_VERSION"
+
+if [ -z "$LATEST_VERSION" ]
+then
+      echo "There is no major/minor/patch commit. Your changes won't be published. Kindly check your commits."
+      exit 1
+else
+      echo "latest version $LATEST_VERSION"
+fi
