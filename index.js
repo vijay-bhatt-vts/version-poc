ac0998928c6d70286e39ea607e58790e5b219a91
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-08 11:02:33 +0530

    bad commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports.helloWorld = function () {
 
     console.log("Hello world main again");
 
-    console.log("major change in staging 1 breaking")
+    console.log("major change in staging 1 breaking");
+
+    console.log("added coding format")
 
   };
