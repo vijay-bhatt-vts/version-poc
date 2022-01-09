f562386ede1dd3e4b146c202be18be4f22183f6d
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-09 18:32:34 +0530

    feat(Resident): ok
    ok
    
    RISE-1234

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ console.log("check pull request");
 
 module.exports.helloWorld = function () {
 
-<<<<<<< HEAD
     console.log("Hello world");
-=======
-    console.log("Hello world main latest 123");
->>>>>>> c72d18c (feat(ok): ok)
 
     console.log("major change in staging 1 breaking");
 
