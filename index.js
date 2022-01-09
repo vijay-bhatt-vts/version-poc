c72d18cf0448b9d31789b3cc3b8c5d48f2db3204
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-09 15:17:36 +0530

    feat(ok): ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("check pull request");
 
 module.exports.helloWorld = function () {
 
-    console.log("Hello world main latest");
+    console.log("Hello world main latest 123");
 
     console.log("major change in staging 1 breaking");
 
