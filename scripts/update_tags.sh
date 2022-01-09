e3429a812efb9b5181f4234c8141c3caf0cc36d0
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-09 15:03:26 +0530

    feat(Resident): ok
    ok
    
    RISE-1234

--- scripts/update_tags.sh
+++ scripts/update_tags.sh
@@ -4,20 +4,24 @@ set -e
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo $branch
-git fetch --unshallow --tags
-# if [ -z "$LATEST_VERSION" ]
-# then
-#       echo "There is no major/minor/patch commit. Your changes won't be published. Kindly check your commits."
-#       exit 1
-# else
-#       echo "latest version $LATEST_VERSION"
-lasttag=$(git for-each-ref --sort=creatordate --format '%(refname) %(creatordate)' refs/tags| grep $branch | tail -1 | cut -d " " -f 1 | cut -d "/" -f 3)
-echo $lasttag
+#git fetch --unshallow --tags
 
-# git tag -d $lasttag
-# git push --delete origin $lasttag  
-# git tag $lasttag
-# git push origin $lasttag
+main="main"
+if [ "$branch" == "$main" ] 
+then
+    lasttag=$(git for-each-ref --sort=creatordate --format '%(refname) %(creatordate)' refs/tags| grep -v - | tail -1 | cut -d " " -f 1 | cut -d "/" -f 3)
+    echo $lasttag
+else
+    lasttag=$(git for-each-ref --sort=creatordate --format '%(refname) %(creatordate)' refs/tags| grep $branch | tail -1 | cut -d " " -f 1 | cut -d "/" -f 3)
+    echo $lasttag
+fi
 
-# git notes --ref semantic-release add -f -m '{"channels":${branch}}' $lasttag
-# git push --force origin refs/notes/semantic-release
+echo "{'channels':$branch}"
+
+git tag -d $lasttag
+git push --delete origin $lasttag  
+git tag $lasttag
+git push origin $lasttag
+
+git notes --ref semantic-release add -f -m "{'channels':$branch}" $lasttag
+git push --force origin refs/notes/semantic-release
