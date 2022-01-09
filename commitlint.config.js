4c0c91e6428f737db48ce05bf3d2b32778bf8d71
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-09 10:27:12 +0530

    feat(Staff): RISE-58820 ticket id
    feature id

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,12 +9,22 @@ module.exports = {extends: ['@commitlint/config-conventional'],
             'perf',
         ],
     ],
-    'scope-empty': [2, 'always'],
-    'references-empty':[2,'never']
+    'subject-case': [
+        0,
+        'never',
+        ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+        2,
+        'always',
+        ['Resident','Staff', 'PM', 'Payment', 'Automation']
+    ]
 },
-parserPreset: {
-    parserOpts: {
-      issuePrefixes: ['RISE-']
-    }
-  }
+prompt: {
+    settings: {
+        'enableMultipleScopes' : true
+    },
+}
+ 
 };
