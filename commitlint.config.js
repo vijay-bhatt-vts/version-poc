38ac4e16d93e98b5d650f987e2bfc0ff0691bff0
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-09 09:35:38 +0530

    feat: ok
    abc def GHI

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,78 +1,44 @@
 module.exports = {extends: ['@commitlint/config-conventional'],
 "rules": {
+    'type-enum': [
+        2,
+        'always',
+        [
+            'feat',
+            'fix',
+            'perf',
+        ],
+    ],
+    'scope-empty': [2, 'always'],
+    'references-empty':[2,'always']
 },
 prompt: {
+    settings:{
+
+    },
     questions: {
         type: {
             description: "Select the type of change that you're committing",
             enum: {
                 feat: {
-                    description: 'A new feature',
+                    description: 'A new feature 123',
                     title: 'Features',
                     emoji: '✨',
                 },
-                fix: {
-                    description: 'A bug fix',
-                    title: 'Bug Fixes',
-                    emoji: '🐛',
-                },
-                // style: {
-                //     description:
-                //         'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-                //     title: 'Styles',
-                //     emoji: '💎',
-                // },
-                // refactor: {
-                //     description:
-                //         'A code change that neither fixes a bug nor adds a feature',
-                //     title: 'Code Refactoring',
-                //     emoji: '📦',
-                // },
-                // perf: {
-                //     description: 'A code change that improves performance',
-                //     title: 'Performance Improvements',
-                //     emoji: '🚀',
-                // },
-                // test: {
-                //     description: 'Adding missing tests or correcting existing tests',
-                //     title: 'Tests',
-                //     emoji: '🚨',
-                // },
-                // build: {
-                //     description:
-                //         'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
-                //     title: 'Builds',
-                //     emoji: '🛠',
-                // },
-                // ci: {
-                //     description:
-                //         'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
-                //     title: 'Continuous Integrations',
-                //     emoji: '⚙️',
-                // },
-                // chore: {
-                //     description: "Other changes that don't modify src or test files",
-                //     title: 'Chores',
-                //     emoji: '♻️',
-                // },
-                // revert: {
-                //     description: 'Reverts a previous commit',
-                //     title: 'Reverts',
-                //     emoji: '🗑',
-                // },
+                
             },
         },
-        // scope: {
-        //     description:
-        //         'What is the scope of this change (e.g. component or file name)',
-        // },
+        scope: {
+            description:
+                'What is the scope of this change (e.g. component or file name)',
+        },
         subject: {
             description:
                 'Write a short, imperative tense description of the change',
         },
-        // body: {
-        //     description: 'Provide a longer description of the change',
-        // },
+        body: {
+            description: 'Provide a longer description of the change',
+        },
         isBreaking: {
             description: 'Are there any breaking changes?',
         },
