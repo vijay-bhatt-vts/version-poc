213648ce80ae2f255a8751706e853d081a7ef68a
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-10 14:54:37 +0530

    fix(Resident): ok
    ok
    
    RISE-12345

--- index.js
+++ index.js
@@ -2,19 +2,7 @@ console.log("check pull request");
 
 module.exports.helloWorld = function () {
 
-    console.log("Hello world main latest 123456");
-
-    console.log("Hello world staging2");
-
-   console.log("Breaking change in main")
-
-    console.log("Hello world 1234");
-
-    console.log("Hello world staging1");
-
-    console.log("Hello world 1234");
-
-    console.log("Hello world staging1");
+   console.log("staging1")
 
    
 
