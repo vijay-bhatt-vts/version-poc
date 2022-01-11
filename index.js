715ef1983e04c358a45f7789e22b447c3106a152
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-11 08:31:44 +0530

    fix(Resident,Staff): ok
    ok
    
    RISE-12345

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ console.log("check pull request");
 module.exports.helloWorld = function () {
 
 
-   console.log("staging workflow 1 push")
+   console.log("staging1 workflow 2 push")
 
-
-   console.log("staging1 workflow1 push")
-
-  
-
-   console.log('final push for workflow 1')
 
   };
