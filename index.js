e85f60658ce14ec8a71acb45b4a1a78b4b3bff34
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-11 17:00:16 +0530

    feat(Resident): ok
    ok
    
    RISE-12345

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports.helloWorld = function () {
 
    console.log("main branch fix major")
 
-    console.log("major2 fix123 abc")
+   console.log("major2 fix123")
+
+   console.log("feature")
 
 
 
