c9e83e700bc733ba041008333ee3f2ea54194054
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-11 21:27:18 +0530

    fix(Resident): ok
    ok
    
    RISE-12345

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ console.log("check pull request");
 module.exports.helloWorld = function () {
 
 
-   console.log("main branch fix major")
-
-    console.log("major2 fix123")
-
-   console.log("feature 1234")
-
-
-<<<<<<< HEAD
-   console.log("feature major change 1")
-=======
-   console.log("major changes")
->>>>>>> 8d24e346aea5cb7cedc2a9b4fb9a5a97e2684402
-
+   console.log("fix")
 
 
 
