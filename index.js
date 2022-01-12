548483244a42e70622d24d00b3dad227bd5ba98d
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-12 13:40:03 +0530

    fix(Resident): ok
    ok
    
    RISE-1234

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports.helloWorld = function () {
 
   console.log("statging and staging1 furthur")
 
-  console.log("change123 one more time")
+  console.log("change123 one more time 123")
 
   };
 
