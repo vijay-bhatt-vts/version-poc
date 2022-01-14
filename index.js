a8ab9dc5a9905c354ede84d7fa3ec38a65acd923
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-14 07:45:31 +0530

    fix(Resident): ok
    ok
    
    RISE-12345

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("check pull request");
 
 module.exports.helloWorld = function () {
 
-console.log("main current state staging1234567890000")
+console.log("main current state staging1234567890000123")
 console.log("main current state hotfix 123")
 
 
