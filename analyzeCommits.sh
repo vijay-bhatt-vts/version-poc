a93901544e6433c2673a43e11de842dc4ca6999d
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-15 22:42:09 +0530

    check

--- analyzeCommits.sh
+++ analyzeCommits.sh
@@ -2,9 +2,16 @@
 
 arr=$1
 
+arraylength=${#arr[@]}
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+  jq -r '.${arr[$i]} | keys[] as $k | "\($k), \(.[$k])"'
+done
+
 for i in "${arr[@]}"
 do
-   echo "$i"
+   jq -r '.myhosts | keys[] as $k | "\($k), \(.[$k] | .ip)"'
    # or do whatever with individual element of the array
 done
 
