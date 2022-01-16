795c5e2c79106e9dd9cc6a4c5b2d96dddd73d7ed
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-16 10:18:15 +0530

    check

--- analyzeCommits.sh
+++ analyzeCommits.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-arr=$1
-
-arraylength=${#arr[@]}
-
-for (( i=0; i<${arraylength}; i++ ));
-do
-  jq -r '.arr[0] | keys[] as $k | "\($k), \(.[$k])"'
-done
+echo 'patch'
 
 
  
