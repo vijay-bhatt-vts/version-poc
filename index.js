5fbba8e766f8e9aba278e3591cca4091fa9e75ee
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-16 12:18:05 +0530

    feat(Resident): ok
    ok
    
    RISE-12345

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.helloWorld = function () {
 console.log("main current state staging")
 console.log("main current state hotfix 1234")
 
-console.log("checking commit 123456")
+console.log("checking commit hello")
 
 
   };
