3b6ff5d4c1c923ce9c714145e4b04fe29d99e907
vijay-bhatt-vts <vijay.bhatt@ext.vts.com>
2022-01-18 14:34:36 +0530

    feat(Resident)
    ok

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,27 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
 "rules": {
-    'subject-case': [
-        0,
-        'never',
-        ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
+    // 'subject-case': [
+    //     0,
+    //     'never',
+    //     ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    // ],
+    'subject-empty': [2, 'always'],
+    "body-empty": [2,'never'],
     'scope-empty': [2, 'never'],
     'scope-enum': [
         2,
         'always',
         ['Resident','Staff', 'PM', 'Payment', 'Automation','release']
     ],
-    'references-empty': [2, 'never']
+    //'references-empty': [2, 'never']
     
 
 },
 parserPreset: {
     parserOpts: {
-        // headerPattern: "/^(\w*)(?:\(([\w\$\.\-\* ]*)\))?\/",
-        // headerCorrespondence: ["type", "scope"],
+         headerPattern: /^(\w*)(?:\(([\w\$\.\-\* ]*)\))/,
+        headerCorrespondence: ["type", "scope"],
         issuePrefixesCaseSensitive : true,
         issuePrefixes: ['RISE-']
     }
